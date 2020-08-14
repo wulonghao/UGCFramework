0d3c26ed974e4f44e6866b6bc46573bdf41134e7
wu <gogs@fake.local>
2020-08-14 11:45:22 +0800

    Test1Node增加热更注册示例

--- ExampleProject/UnityProject/Assets/Scripts/Nodes/Test1Node.cs
+++ ExampleProject/UnityProject/Assets/Scripts/Nodes/Test1Node.cs
@@ -11,6 +11,29 @@ public class Test1Node : Node
     public override void Init()
     {
         base.Init();
+        if (CheckHotFixMethod(out string methodName))
+        {
+            InvokeHotFix(methodName, null);
+            return;
+        }
         UGUIEventListener.Get(btnTest2Node).onClick = delegate { NodeManager.OpenNode<Test2Node>(); };
+    }
+
+    public void TempHotUpdate1(int test)
+    {
+        if (CheckHotFixMethod(out string methodName))
+        {
+            InvokeHotFix(methodName, test);
+            return;
+        }
+    }
+
+    public static void TempHotUpdate2(int test)
+    {
+        if (CheckHotFixStaticMethod(out string typeFullName, out string methodName))
+        {
+            InvokeStaticHotFix(typeFullName, methodName, test);
+            return;
+        }
     }
 }
