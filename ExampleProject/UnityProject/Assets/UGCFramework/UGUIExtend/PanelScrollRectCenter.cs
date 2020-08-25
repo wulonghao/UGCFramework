2fdbfd6af5ed4d029a568e0cbf4e481ff8b51b83
wu <gogs@fake.local>
2020-08-25 13:54:10 +0800

    优化滑动面板组件

--- ExampleProject/UnityProject/Assets/UGCFramework/UGUIExtend/PanelScrollRectCenter.cs
+++ ExampleProject/UnityProject/Assets/UGCFramework/UGUIExtend/PanelScrollRectCenter.cs
@@ -3,6 +3,7 @@ using UGCF.Manager;
 using UGCF.Utils;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace UGCF.UGUIExtend
 {
@@ -16,97 +17,108 @@ namespace UGCF.UGUIExtend
         public bool vertical;
         public bool horizontal;
         public bool defaultFirst = true;
-        public bool isAutoLoop, isCircle;
+        public bool isCircle, isAutoLoop;
         public float loopTimeSpace = 5;
         public float dragThresholdTime = 0.3f;//拖拽检测时间阈值，低于阈值取最后一帧操作判断行为，否则以最近元素为目标元素
         public int speed = 10;//拖拽检测时间阈值，低于阈值取最后一帧操作判断行为，否则以最近元素为目标元素
         public RectTransform content;
+        bool moving;
         float startDragTime;
         float screenToCanvasScale;
         int currentIndex = -1;
-        Coroutine coroutine;
+        Coroutine coroutineLoopPlay;
 
         void Start()
         {
             if (isAutoLoop)
-                coroutine = UGCFMain.Instance.StartCoroutine(LoopPlay());
-            screenToCanvasScale = FindObjectOfType<Canvas>().GetComponent<RectTransform>().localScale.x;
+                coroutineLoopPlay = UGCFMain.Instance.StartCoroutine(LoopPlay());
+            screenToCanvasScale = GetComponentInParent<Canvas>().GetComponent<RectTransform>().localScale.x;
         }
 
         void OnEnable()
         {
-            if (defaultFirst)
-                StartCoroutine(DelayInit());
+            StartCoroutine(DelayInit());
         }
 
         IEnumerator DelayInit()
         {
             yield return WaitForUtils.WaitFrame;
-            SetCenter(GetCenter(), false);
+            if (defaultFirst)
+                SetCenter(null, false);
+            else
+                SetCenter(GetCenter(), false);
         }
 
         public virtual void OnBeginDrag(PointerEventData eventData)
         {
+            if (!horizontal && !vertical)
+                return;
             startDragTime = Time.realtimeSinceStartup;
             moving = false;
-            if (coroutine != null)
-                EternalGameObject.Instance.StopCoroutine(coroutine);
+            RefreshCircle(eventData.delta);
+            if (coroutineLoopPlay != null)
+                UGCFMain.Instance.StopCoroutine(coroutineLoopPlay);
         }
 
         public virtual void OnDrag(PointerEventData eventData)
         {
-            if (horizontal || vertical)
+            if (horizontal && vertical)
             {
-                if (horizontal)
-                {
-                    content.localPosition += Vector3.right * eventData.delta.x / screenToCanvasScale;
-                    ValueChange(eventData.delta.x);
-                }
-                if (vertical)
-                {
-                    content.localPosition += Vector3.up * eventData.delta.y / screenToCanvasScale;
-                    ValueChange(eventData.delta.y);
-                }
+                content.localPosition += new Vector3(eventData.delta.x / screenToCanvasScale, eventData.delta.y / screenToCanvasScale);
+            }
+            else if (horizontal)
+            {
+                content.localPosition += Vector3.right * eventData.delta.x / screenToCanvasScale;
+                CircleChange(eventData.delta.x);
+            }
+            else if (vertical)
+            {
+                content.localPosition += Vector3.up * eventData.delta.y / screenToCanvasScale;
+                CircleChange(eventData.delta.y);
+            }
+            else
+                return;
 
-                for (int i = 0; i < content.childCount; i++)
-                {
-                    RectTransform crtf = content.GetChild(i).GetComponent<RectTransform>();
-                    Vector2 diff = crtf.localPosition - transform.localPosition;
-                    float scale = 0;
-                    if (horizontal)
-                        scale = Mathf.Abs(diff.x) / crtf.rect.width;
-                    else if (vertical)
-                        scale = Mathf.Abs(diff.y) / crtf.rect.height;
-                    if (scale > 1)
-                        scale = 1;
-                    onValueChanged?.Invoke(crtf.gameObject, scale);
-                }
+            for (int i = 0; i < content.childCount; i++)
+            {
+                RectTransform crtf = content.GetChild(i).GetComponent<RectTransform>();
+                Vector2 diff = crtf.localPosition - transform.localPosition;
+                float scale = 0;
+                if (horizontal)
+                    scale = Mathf.Abs(diff.x) / crtf.rect.width;
+                else if (vertical)
+                    scale = Mathf.Abs(diff.y) / crtf.rect.height;
+                if (scale > 1)
+                    scale = 1;
+                onValueChanged?.Invoke(crtf.gameObject, scale);
             }
         }
 
         public virtual void OnEndDrag(PointerEventData eventData)
         {
+            if (!horizontal && !vertical)
+                return;
             Transform centerTf = GetCenter();
             if (!centerTf || !content)
                 return;
-            if (Time.realtimeSinceStartup - startDragTime < dragThresholdTime)
+            if (Time.realtimeSinceStartup - startDragTime < dragThresholdTime && !(horizontal && vertical))
             {
                 Transform targetTf = null;
                 if (horizontal)
                 {
                     if (eventData.delta.x < 0)
-                        targetTf = GetRightEnableTransform(centerTf);
+                        targetTf = GetNextEnableItem(centerTf);
                     else if (eventData.delta.x > 0)
-                        targetTf = GetLeftEnableTransform(centerTf);
+                        targetTf = GetLastEnableItem(centerTf);
                     else
                         targetTf = centerTf;
                 }
                 else if (vertical)
                 {
                     if (eventData.delta.y > 0)
-                        targetTf = GetRightEnableTransform(centerTf);
+                        targetTf = GetNextEnableItem(centerTf);
                     else if (eventData.delta.y < 0)
-                        targetTf = GetLeftEnableTransform(centerTf);
+                        targetTf = GetLastEnableItem(centerTf);
                     else
                         targetTf = centerTf;
                 }
@@ -118,49 +130,63 @@ namespace UGCF.UGUIExtend
             {
                 SetCenter(centerTf);
             }
-            if (coroutine != null)
-                coroutine = UGCFMain.Instance.StartCoroutine(LoopPlay());
+            if (coroutineLoopPlay != null)
+                coroutineLoopPlay = UGCFMain.Instance.StartCoroutine(LoopPlay());
         }
 
-        void ValueChange(float scaleSpaceToCenter)
+        void RefreshCircle(Vector2 direction)
         {
-            if (!isCircle)
+            if (horizontal)
+                CircleChange(direction.x);
+            else if (vertical)
+                CircleChange(direction.y);
+        }
+
+        void CircleChange(float scaleSpaceToCenter)
+        {
+            if (!isCircle || (horizontal && vertical))
                 return;
             if (scaleSpaceToCenter == 0)
                 return;
             RectTransform rtfFirst = content.GetChild(0).GetComponent<RectTransform>();
             RectTransform rtfLast = content.GetChild(content.childCount - 1).GetComponent<RectTransform>();
+            RectTransform contentParent = content.parent.GetComponent<RectTransform>();
             if (horizontal)
             {
-                if (scaleSpaceToCenter < 0 && Mathf.Abs(rtfFirst.transform.position.x - transform.position.x) * screenToCanvasScale > rtfFirst.rect.width)
+                HorizontalLayoutGroup horizontalLayout = content.GetComponent<HorizontalLayoutGroup>();
+                if (scaleSpaceToCenter < 0
+                    && Mathf.Abs(rtfLast.transform.position.x - content.parent.position.x) * screenToCanvasScale < Mathf.Max(rtfLast.rect.width, contentParent.rect.width) * 0.5f)
                 {
                     rtfFirst.SetAsLastSibling();
-                    content.localPosition += Vector3.right * rtfFirst.rect.width;
+                    content.localPosition += Vector3.right * (rtfFirst.rect.width + (horizontalLayout ? horizontalLayout.spacing : 0));
                 }
-                else if (scaleSpaceToCenter > 0 && Mathf.Abs(rtfLast.transform.position.x - transform.position.x) * screenToCanvasScale > rtfLast.rect.width)
+                else if (scaleSpaceToCenter > 0 
+                    && Mathf.Abs(rtfFirst.transform.position.x - content.parent.position.x) * screenToCanvasScale < Mathf.Max(rtfFirst.rect.width, contentParent.rect.width) * 0.5f)
                 {
                     rtfLast.SetAsFirstSibling();
-                    content.localPosition += Vector3.left * rtfLast.rect.width;
+                    content.localPosition += Vector3.left * (rtfLast.rect.width + (horizontalLayout ? horizontalLayout.spacing : 0));
                 }
             }
             else if (vertical)
             {
-                if (scaleSpaceToCenter > 0 && Mathf.Abs(rtfFirst.transform.position.y - transform.position.y) * screenToCanvasScale > rtfFirst.rect.height)
+                VerticalLayoutGroup verticalLayout = content.GetComponent<VerticalLayoutGroup>();
+                if (scaleSpaceToCenter > 0 
+                    && Mathf.Abs(rtfLast.transform.position.y - content.parent.position.y) * screenToCanvasScale < Mathf.Max(rtfLast.rect.height, contentParent.rect.height) * 0.5f)
                 {
                     rtfFirst.SetAsLastSibling();
-                    content.localPosition += Vector3.down * rtfLast.rect.height;
+                    content.localPosition += Vector3.down * (rtfFirst.rect.height + (verticalLayout ? verticalLayout.spacing : 0));
                 }
-                else if (scaleSpaceToCenter < 0 && Mathf.Abs(rtfLast.transform.position.y - transform.position.y) * screenToCanvasScale > rtfLast.rect.height)
+                else if (scaleSpaceToCenter < 0 
+                    && Mathf.Abs(rtfFirst.transform.position.y - content.parent.position.y) * screenToCanvasScale < Mathf.Max(rtfFirst.rect.height, contentParent.rect.height) * 0.5f)
                 {
                     rtfLast.SetAsFirstSibling();
-                    content.localPosition += Vector3.up * rtfFirst.rect.height;
+                    content.localPosition += Vector3.up * (rtfLast.rect.height + (verticalLayout ? verticalLayout.spacing : 0));
                 }
             }
         }
 
-        Transform GetLeftEnableTransform(Transform current)
+        Transform GetLastEnableItem(Transform current)
         {
-            Transform tf = null;
             int targetIndex = current.GetSiblingIndex() - 1;
             while (targetIndex >= 0)
             {
@@ -170,12 +196,11 @@ namespace UGCF.UGUIExtend
                 else
                     targetIndex--;
             }
-            return tf;
+            return current;
         }
 
-        Transform GetRightEnableTransform(Transform current)
+        Transform GetNextEnableItem(Transform current)
         {
-            Transform tf = null;
             int targetIndex = current.GetSiblingIndex() + 1;
             while (content.childCount > targetIndex)
             {
@@ -185,7 +210,7 @@ namespace UGCF.UGUIExtend
                 else
                     targetIndex++;
             }
-            return tf;
+            return current;
         }
 
         RectTransform GetCenter()
@@ -196,8 +221,8 @@ namespace UGCF.UGUIExtend
                 Transform tf = content.GetChild(i);
                 if (!tf.gameObject.activeInHierarchy)
                     continue;
-                if (!centerTf || Vector2.SqrMagnitude(tf.localPosition + content.localPosition)
-                    < Vector2.SqrMagnitude(centerTf.localPosition + content.localPosition))
+                if (!centerTf ||
+                    Vector2.SqrMagnitude(tf.localPosition + content.localPosition) < Vector2.SqrMagnitude(centerTf.localPosition + content.localPosition))
                 {
                     centerTf = tf;
                 }
@@ -216,35 +241,44 @@ namespace UGCF.UGUIExtend
         {
             if (content.childCount == 0)
                 return false;
-            if (!center) center = content.GetChild(0).GetComponent<RectTransform>();
-            startMovingPosition = content.localPosition;
-            if (horizontal)
+
+            if (!center)
+                center = content.GetComponentsInChildren<RectTransform>(false)[1];
+
+            Vector2 startMovingPosition = content.localPosition;
+            Vector2 targetMovingPosition = content.localPosition;
+
+            if (horizontal && vertical)
+                targetMovingPosition = new Vector2(-center.localPosition.x, -center.localPosition.y);
+            else if (horizontal)
                 targetMovingPosition = new Vector2(-center.localPosition.x, startMovingPosition.y);
-            if (vertical)
+            else if (vertical)
                 targetMovingPosition = new Vector2(startMovingPosition.x, -center.localPosition.y);
+
             if (isPlayAnimation)
-                UGCFMain.Instance.StartCoroutine(Moving());
+                StartCoroutine(Moving(startMovingPosition, targetMovingPosition));
             else
                 content.localPosition = targetMovingPosition;
+
             onItemChanged?.Invoke(center.gameObject);
             currentIndex = center.GetSiblingIndex();
             return true;
         }
 
-        bool moving = false;
-        Vector2 startMovingPosition;
-        Vector2 targetMovingPosition;
-        IEnumerator Moving()
+        IEnumerator Moving(Vector2 startMovingPosition, Vector2 targetMovingPosition)
         {
             moving = true;
             float t = 0;
             while (moving)
             {
-                if (!this || !content)
-                    break;
-                content.localPosition = Vector2.Lerp(startMovingPosition, targetMovingPosition, t);
+                if (this && content)
+                    content.localPosition = Vector2.Lerp(startMovingPosition, targetMovingPosition, Mathf.Clamp01(t));
                 if (t >= 1)
+                {
+                    moving = false;
+                    RefreshCircle(targetMovingPosition - startMovingPosition);
                     break;
+                }
                 t += Time.deltaTime * speed;
                 yield return WaitForUtils.WaitFrame;
             }
@@ -259,7 +293,10 @@ namespace UGCF.UGUIExtend
                 do
                 {
                     if (currentIndex + 1 >= content.childCount)
-                        currentIndex = 0;
+                    {
+                        if (!isCircle)
+                            currentIndex = 0;
+                    }
                     else
                         currentIndex++;
                 }
